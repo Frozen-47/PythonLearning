c506d891a0b6c921a10a9feeec51e3e25fc15f36
Sabareesh G <sabareeshgm47@gmail.com>
2026-07-01 10:51:46 +0530

    pipe capitalized o/p

--- 15Phase1/pipe.py
+++ 15Phase1/pipe.py
@@ -1,8 +1,8 @@
 day = input("Enter Day : ")
 match day:
     case "sunday" | "saturday":
-        print(f"{day} is holiday")
+        print(f"{day.capitalize()} is holiday")
     case "wednesday":
-        print(f"{day} mid of the day")
+        print(f"{day.capitalize()} mid of the day")
     case _:
         print("Remaining days")
